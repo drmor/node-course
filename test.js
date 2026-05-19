80d88119167b46d152b70dbec8ae60996a9517f1
ne_uchi <drmor107@gmail.com>
2026-05-19 23:42:18 +0500

    piping

--- test.js
+++ test.js
@@ -3,9 +3,12 @@ const fs = require('fs')
 const readStream = fs.createReadStream('./text.txt', {encoding: 'utf8'});
 const writeStream = fs.createWriteStream('./new_text.txt');
 
-readStream.on('data', (chunk)=>{
-    console.log('new chunk')
-    console.log(chunk)
-    writeStream.write('\nNew chunk\n')
-    writeStream.write(chunk)
-})
+// readStream.on('data', (chunk)=>{
+//     console.log('new chunk')
+//     console.log(chunk)
+//     writeStream.write('\nNew chunk\n')
+//     writeStream.write(chunk)
+// })
+
+//piping (the same as above just shorter)
+readStream.pipe(writeStream)
