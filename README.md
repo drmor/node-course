3528cac7f9d8e733fe95dfeaaf0bb268e6c206fb
ne_uchi <drmor107@gmail.com>
2026-05-19 23:23:16 +0500

    managing files with node

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# node-course
+hello world
--- test.js
+++ test.js
@@ -1,6 +1,42 @@
-const nums = [1, 2, 3, 4, 5]
-const letters = ['a', 'b', 'c', 'd', 'e']
+const fs = require('fs')
 
-module.exports = {
-    nums, letters
-}
+// reading files
+// fs.readFile('./README.md', (error, data) => {
+//     if (error) {
+//         console.log(error)
+//     }
+//     console.log(data.toString())
+// })
+
+// writing files
+// fs.writeFile('./README2.md', 'hello world', () => {
+//     console.log('file written')
+// })
+
+// directories
+// if (!fs.existsSync('./assets')){
+//     fs.mkdir('./assets', (error) =>{
+//         if (error) {
+//             console.log(error)
+//         }
+//         console.log("folder created")
+//     })
+// } else {
+//     fs.rmdir('./assets', (error) =>{
+//         if (error) {
+//             console.log(error)
+//         }
+//         console.log('folder deleted')
+//     })
+// }
+
+
+// deleting files
+// if(fs.existsSync('./README2.md')){
+//     fs.unlink('./README2.md', (error)=>{
+//         if (error) {
+//             console.log(error)
+//         }
+//         console.log('file deleted')
+//     })
+// }
