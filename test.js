365b8b93d04255e82b4e6b19ecebcffd996bcef9
ne_uchi <drmor107@gmail.com>
2026-05-19 23:37:48 +0500

    reading streams

--- test.js
+++ test.js
@@ -1,42 +1,8 @@
 const fs = require('fs')
 
-// reading files
-// fs.readFile('./README.md', (error, data) => {
-//     if (error) {
-//         console.log(error)
-//     }
-//     console.log(data.toString())
-// })
+const readStream = fs.createReadStream('./text.txt', {encoding: 'utf8'});
 
-// writing files
-// fs.writeFile('./README2.md', 'hello world', () => {
-//     console.log('file written')
-// })
-
-// directories
-// if (!fs.existsSync('./assets')){
-//     fs.mkdir('./assets', (error) =>{
-//         if (error) {
-//             console.log(error)
-//         }
-//         console.log("folder created")
-//     })
-// } else {
-//     fs.rmdir('./assets', (error) =>{
-//         if (error) {
-//             console.log(error)
-//         }
-//         console.log('folder deleted')
-//     })
-// }
-
-
-// deleting files
-// if(fs.existsSync('./README2.md')){
-//     fs.unlink('./README2.md', (error)=>{
-//         if (error) {
-//             console.log(error)
-//         }
-//         console.log('file deleted')
-//     })
-// }
+readStream.on('data', (chunk)=>{
+    console.log('new chunk')
+    console.log(chunk)
+})
