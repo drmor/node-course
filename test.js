ce5872c89a94e9b32f88509c5f1b12617c8f5d23
ne_uchi <drmor107@gmail.com>
2026-05-19 23:40:30 +0500

    writing chunks in new file from stream

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
 
 const readStream = fs.createReadStream('./text.txt', {encoding: 'utf8'});
+const writeStream = fs.createWriteStream('./new_text.txt');
 
 readStream.on('data', (chunk)=>{
     console.log('new chunk')
     console.log(chunk)
+    writeStream.write('\nNew chunk\n')
+    writeStream.write(chunk)
 })
