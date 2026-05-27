c176a5fbc03efad990fb17f8ee57a2ab7ebc6ad9
ne_uchi <drmor107@gmail.com>
2026-05-27 21:37:31 +0500

    res diff files with status code

--- server.js
+++ server.js
@@ -8,13 +8,29 @@ const server = http.createServer((req, res) => {
 
   res.setHeader('Content-Type', 'text/html');
 
+  let path = './views/';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+      break;
+  }
+
   // sent an html file
-  fs.readFile('./views/index.html', (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
     } else {
-      // res.write(data); we can use end with inly one file
+      // res.write(data); we can use end() with only one file
       res.end(data);
     }
   });
