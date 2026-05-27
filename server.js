ad128d05861afe3b66078a5b99a35037e3e03cef
ne_uchi <drmor107@gmail.com>
2026-05-27 21:08:26 +0500

    simple res

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('request made');
+  console.log(req.url, req.method);
+
+  // set header content type
+
+  res.setHeader('Content-Type', 'text/html');
+
+  res.write('<p>Hello, world</p>');
+  res.write('<p>Hello, world ver 2</p>');
+  res.end();
 });
 
 server.listen(3000, 'localhost', () => {
