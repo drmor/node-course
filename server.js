04586a2d5a3e1bebc85df9e25591cef8222d03e3
ne_uchi <drmor107@gmail.com>
2026-05-27 21:15:11 +0500

    res an html file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
@@ -7,9 +8,16 @@ const server = http.createServer((req, res) => {
 
   res.setHeader('Content-Type', 'text/html');
 
-  res.write('<p>Hello, world</p>');
-  res.write('<p>Hello, world ver 2</p>');
-  res.end();
+  // sent an html file
+  fs.readFile('./views/index.html', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data); we can use end with inly one file
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, 'localhost', () => {
