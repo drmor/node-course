581283b43ec89027efe226f2efedcbc7f467a18c
ne_uchi <drmor107@gmail.com>
2026-05-30 20:28:50 +0500

    express

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+//listen for requests
+
+app.listen(3000);
+
+app.get('/', (req, res) => {
+  res.send('<p>home page</p>');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<p>about page</p>');
+});
