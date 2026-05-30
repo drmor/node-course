51b7d28a8c78f0ed4bb5ec31f6d14a540beee0b6
ne_uchi <drmor107@gmail.com>
2026-05-30 20:59:44 +0500

    ejs full site

--- app.js
+++ app.js
@@ -11,20 +11,19 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
   //res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
   //res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about');
 });
 
-//redirect
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+  res.render('create');
 });
 
 //404
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404');
 });
